9e48db93e3012a466b48d9cfd5eac63f08103228
Taylor Bradley <trbradley48@gmail.com>
2019-06-03 16:55:46 -0700

    functioning program with no visuals

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheeseburger', false);
